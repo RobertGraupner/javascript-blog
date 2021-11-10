de399bc2f4021c8da3b2e3d524bdd58e22738b17
Robert Graupner <robert.graupnerr@gmail.com>
2021-11-10 18:51:21 +0100

    add code to add classes and find value of 'href'

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@
 });*/
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked');
+
   /* remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -14,8 +16,10 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   } 
   /* add class 'active' to the clicked link */
+
   clickedElement.classList.add('active');
   console.log('clickedElement:' , clickedElement);
+
   /* remove class 'active' from all articles */
   
   const activeArticles = document.querySelectorAll('.posts article.active');
@@ -23,11 +27,19 @@ const titleClickHandler = function(event){
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   } 
+
   /* get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector); 
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
