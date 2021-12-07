407a50dc517be0bc8ae9700a247238cfa7155dc9
Robert Graupner <robert.graupnerr@gmail.com>
2021-12-07 18:44:10 +0100

    modify generateTags function

--- js/script.js
+++ js/script.js
@@ -46,7 +46,8 @@ const optArticleSelector = '.post',
 optTitleSelector = '.post-title',
 optTitleListSelector = '.titles',
 optArticleTagsSelector = '.post-tags .list',
-optArticleAuthorSelector = '.post-author';
+optArticleAuthorSelector = '.post-author',
+optTagsListSelector = '.tags';
 
 function generateTitleLinks(customSelector = ''){
 
@@ -92,12 +93,14 @@ function generateTitleLinks(customSelector = ''){
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
   for(let article of articles){
     /* find tags wrapper */
-    const tagList = article.querySelector(optArticleTagsSelector);
+    const articleList = article.querySelector(optArticleTagsSelector);
     /* make html variable with empty string */
     let html = '';
     /* get tags from data-tags attribute */
@@ -110,14 +113,26 @@ function generateTags(){
       const linkHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
       /* add generated code to html variable */
       html = html + linkHTML + ' ';
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     }
     /* END LOOP: for each tag */
 
     /* insert HTML of all the links into the tags wrapper */
-    tagList.innerHTML = html;
+    articleList.innerHTML = html;
   }
   /* END LOOP: for every article: */
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
+
+
 
 generateTags();
 
